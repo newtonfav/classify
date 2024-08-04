0cd4baf327f45a100b9137363c20f96138567369
Favour Oghenekowho <gh.oghene@gmail.com>
2024-08-04 18:37:09 +0100

    fix error

--- app/inventory/error.tsx
+++ app/inventory/error.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 
-export default function Error({ error, reset }) {
+export default function Error({ error, reset }: any) {
   return (
     <main className="flex justify-center items-center flex-col gap-6">
       <h1 className="text-3xl font-semibold">Something went wrong!</h1>
